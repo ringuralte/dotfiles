1732823d66101103055a12c47ff0eabace233f02
ringuralte <ringuralte@protonmail.com>
2023-11-23 15:22:30 +0530

    Change colorscheme and handle updates

--- dot_config/nvim/lua/ringu/plugins/colorscheme.lua
+++ dot_config/nvim/lua/ringu/plugins/colorscheme.lua
@@ -1,11 +1,4 @@
 return {
-  -- also consider onedark and one dark pro nvim
-  -- 'olimorris/onedarkpro.nvim',
-  -- priority = 1000,
-  -- config = function()
-  --   require('onedarkpro').setup()
-  --   vim.cmd [[colorscheme onedark_vivid]]
-  -- end,
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
