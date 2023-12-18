738ad56eead75eff0da1133bf83a104b3d0c2f93
ringuralte <ringuralte@protonmail.com>
2023-12-18 14:41:30 +0530

    use double quotes, remove tabbar

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,33 +7,36 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Dracula'
+config.color_scheme = "Dracula"
 
 -- Font
-config.font = wezterm.font 'Iosevka Term SS14 Medium'
+config.font = wezterm.font("Iosevka Term SS14 Medium")
+
 -- config.font = wezterm.font 'JetBrains Mono NF Medium'
 config.font_size = 12.0
 
+-- set window decorations
+config.window_decorations = "INTEGRATED_BUTTONS|NONE"
+
 -- keys
 config.keys = {
-  {
-    key = '{',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Left',
-  },
-  {
-    key = '}',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Right',
-  },
+	{
+		key = "{",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "}",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
 }
 
 -- and finally, return the configuration to wezterm
 return config
-
