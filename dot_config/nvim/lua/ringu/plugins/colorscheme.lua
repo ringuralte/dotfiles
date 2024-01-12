1e89518e0876b173ac7a76d457ba9e166cf42a69
ringuralte <ringuralte@protonmail.com>
2024-01-12 15:01:15 +0530

    Change theme to catpuccin

--- dot_config/nvim/lua/ringu/plugins/colorscheme.lua
+++ dot_config/nvim/lua/ringu/plugins/colorscheme.lua
@@ -1,16 +1,26 @@
 return {
-	-- 'catppuccin/nvim',
-	-- name = 'catppuccin',
-	-- priority = 1000,
-	-- config = function()
-	--   require('catppuccin').setup()
-	--   vim.cmd [[colorscheme catppuccin-macchiato]]
-	-- end,
-	"Mofiqul/dracula.nvim",
-	name = "dracula",
+	'catppuccin/nvim',
+	name = 'catppuccin',
 	priority = 1000,
 	config = function()
-		require("dracula").setup()
-		vim.cmd ([[colorscheme dracula]])
+	  require('catppuccin').setup()
+	  vim.cmd [[colorscheme catppuccin-macchiato]]
 	end,
+	-- "Mofiqul/dracula.nvim",
+	-- name = "dracula",
+	-- priority = 1000,
+	-- config = function()
+	-- 	require("dracula").setup()
+	-- 	vim.cmd ([[colorscheme dracula]])
+	-- end,
+	-- "rose-pine/neovim",
+	-- name = "rose-pine",
+	-- priority = 1000,
+	-- config = function()
+	-- 	require("rose-pine").setup({
+	-- 		variant = "moon",
+	-- 		dark_variant = "moon"
+	-- 	})
+	-- 	vim.cmd ([[colorscheme rose-pine]])
+	-- end,
 }
