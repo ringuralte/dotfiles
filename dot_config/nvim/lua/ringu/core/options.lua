4ea82620bfd535939ee4e28733e96816c5589682
ringuralte <ringuralte@protonmail.com>
2024-01-16 16:00:04 +0530

    Add vim filetype extensions for postcss and pcss files to be css

--- dot_config/nvim/lua/ringu/core/options.lua
+++ dot_config/nvim/lua/ringu/core/options.lua
@@ -54,3 +54,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+vim.filetype.add({
+  extension = {
+    postcss = 'css',
+    pcss = 'css'
+  }
+})
